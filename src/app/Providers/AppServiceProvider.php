589238c05f5f7d42df68bcf11a23244591234ce0
Théo Vuilliomenet <theo.vuilliomenet@he-arc.ch>
2022-11-01 17:14:16 +0100

    fix mixed content problem

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https')['https://coursmanager2000.k8s.ing.he-arc.ch'];
+        }
     }
 }
