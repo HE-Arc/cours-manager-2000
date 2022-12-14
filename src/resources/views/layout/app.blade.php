60190aecb23ed9cf084ba8a6ec67b95f81c69fb3
Théo Vuilliomenet <theo.vuilliomenet@he-arc.ch>
2022-12-14 18:58:55 +0100

    Change alert style

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -61,8 +61,11 @@
     <!-- Begin page content -->
     <div class="container mt-3">
         @if ($message = Session::get('success'))
-            <div class="alert alert-success">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <p>{{ $message }}</p>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
         @endif
         @yield('content')
